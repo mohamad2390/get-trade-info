f6f2b3796438bb932c0dc0622e65de0184f4aacc
mohamad <mohamad23902390@gmail.com>
2024-08-16 23:00:21 +0330

    write the info file

--- app/Http/Controllers/CheckController.js
+++ app/Http/Controllers/CheckController.js
@@ -8,11 +8,12 @@ exports.check = async (req, res) => {
         console.log(positionInfo)
         var text = positionInfo.text+"\n"
         const fs = require('fs');
-    fs.appendFile("internal_server_info.txt", text, function (err) {
-        if (err) {
-            console.log(111111111)
-        }
-    });
+    // fs.appendFile("internal_server_info.txt", text, function (err) {
+    //     if (err) {
+    //         console.log(111111111)
+    //     }
+    // });
+    fs.writeFile("internal_server_info.txt", text, function(){console.log('done')})
     res.status(200).send('ok').end();
         
     } catch (error) {
